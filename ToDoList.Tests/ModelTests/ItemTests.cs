35435774c197c26cb92e7eed24907c5c0b92a8f4
Christen Weston <paleeserecycle@gmail.com>
2022-02-03 20:41:14 -0800

    Second test written with good fail

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -14,5 +14,18 @@ namespace ToDoList.Tests
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
 
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      
+      //Act
+      string result = newItem.Description;
+      
+      //Assert
+      Assert.AreEqual(description, result);
+    }
   }
 }
