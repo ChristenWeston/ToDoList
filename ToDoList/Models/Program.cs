2a0a3ab8616c72fa95856173a91e31ee80e67b0c
Christen Weston <paleeserecycle@gmail.com>
2022-02-06 11:48:24 -0800

    Initial program main working and returning and exiting as expected

--- ToDoList/Models/Program.cs
+++ ToDoList/Models/Program.cs
@@ -7,30 +7,47 @@ namespace ListMaker
   public class Program
   {
     
-    public static void Main()
-    {
-      Console.WriteLine("Welcome to the To Do List!");
-      Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-      string viewOrAddToList = Console.ReadLine();
-
+     public static void Main()
+     {
+       Console.WriteLine("Welcome to the To Do List Main Menu!");
 //Depending on if user says "Add" or "View", add new item to list or view the entire list
-//Is this a method?
-      //public static void UserChoice()
-      //{
+      {
+        Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
+        string viewOrAddToList = Console.ReadLine();
         if (viewOrAddToList == "Add")
         {
           Console.WriteLine("What would you like to add to your to do list? Enter description: ");
           string newItemDescription = Console.ReadLine();
           Item newItem1 = new Item(newItemDescription);
-          Console.WriteLine(newItemDescription + " was added to the list. Would you like to add another item to your list? (Add/View)");
-         // return UserChoice;
+          Console.WriteLine(newItemDescription + " was added to the list!");
+          Console.WriteLine("Do you want to leave the to do list? ('Y' for yes)");
+          string leaveOrStay = Console.ReadLine();
+          if (leaveOrStay == "Y" || leaveOrStay == "y")
+          {
+            Console.WriteLine("Ok bye bye!");
+          }
+          else
+          {
+            Main();
+          }
         }
         else if (viewOrAddToList == "View")
         {
           List<Item> result = Item.GetAll();
           Console.WriteLine(result);
+          Console.WriteLine("Did you view your list?");
+          Console.WriteLine("Do you want to leave the do do list? ('Y' for yes");
+          string leaveOrStay = Console.ReadLine();
+          if (leaveOrStay == "Y" || leaveOrStay == "y")
+          {
+            Console.WriteLine("Ok bye bye!");
+          }
+          else
+          {
+            Main();
+          }
         }
-      //}
+      }
     }
   }
 }
