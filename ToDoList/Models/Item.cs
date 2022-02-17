863ef217e3c77ab7ccbe4274ff00e8eed1e8718a
Christen Weston <paleeserecycle@gmail.com>
2022-02-16 19:25:08 -0800

    All 7 tests passing

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,6 +6,7 @@ namespace ToDoList.Models
   {
 
     public string Description { get; set; }
+    public int Id { get; }
     private static List<Item> _instances = new List<Item> {};
 
     public Item(string description)
@@ -13,6 +14,7 @@ namespace ToDoList.Models
       Description = description;
       //Every time an item is created, add it to the List
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
 //Variables and methods dealing with entire classes must be static
@@ -24,6 +26,11 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
   }
 }
