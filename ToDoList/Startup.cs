f32104da6ffb89762c88f7f4df0be4ccbd8e5639
Christen Weston <paleeserecycle@gmail.com>
2022-02-27 16:13:40 -0800

    Updated project to use EF Core with database

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
 using ToDoList.Models;
 
 namespace ToDoList
@@ -12,10 +12,10 @@ namespace ToDoList
   {
     public Startup(IWebHostEnvironment env)
     {
-    var builder = new ConfigurationBuilder()
-        .SetBasePath(env.ContentRootPath)
-        .AddJsonFile("appsettings.json");
-    Configuration = builder.Build();
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddJsonFile("appsettings.json");
+      Configuration = builder.Build();
     }
 
     public IConfigurationRoot Configuration { get; set; }
@@ -23,27 +23,28 @@ namespace ToDoList
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+
       services.AddEntityFrameworkMySql()
-      .AddDbContext<ToDoListContext>(options => options
-      .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+        .AddDbContext<ToDoListContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
       app.UseRouting();
-       app.UseStaticFiles();
 
       app.UseEndpoints(routes =>
       {
         routes.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
       });
 
+      app.UseStaticFiles();
+      
       app.Run(async (context) =>
       {
         await context.Response.WriteAsync("Hello World!");
       });
     }
   }
-
 }
