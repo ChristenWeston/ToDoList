3d8c54e2b49542526c4cd684ab3d52c4a4f7c62e
Christen Weston <paleeserecycle@gmail.com>
2022-03-13 10:35:07 -0700

    Adding in navigation properties

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -6,15 +6,11 @@ namespace ToDoList.Models
     {
         public Category()
         {
-          //HashSet is an unordered collection of unique elements. HashSet cannot have duplicates
-            this.Items = new HashSet<Item>();
+            this.JoinEntities = new HashSet<CategoryItem>();    
         }
 
         public int CategoryId { get; set; }
         public string Name { get; set; }
-        //Declare Items as an instance of ICollection - generic interface built into the .NET framework
-        //ICollection has methods for querying and changing data
-        //Declaring as virtual to allow Entity to use lazy loading
-        public virtual ICollection<Item> Items { get; set; }
+        public virtual ICollection<CategoryItem> JoinEntities { get; set; }
     }
 }
