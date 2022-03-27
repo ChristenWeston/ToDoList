6283c37f2dcd6d39eb0d1bc8933ab8be781d36fe
Christen Weston <paleeserecycle@gmail.com>
2022-03-27 12:15:25 -0700

    Added in User to Item class

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -12,6 +12,7 @@ namespace ToDoList.Models
         public int ItemId { get; set; }
         public string Description { get; set; }
         public bool Completed { get; set; }
+        public virtual ApplicationUser User { get; set; } //Declared virtual to allow lazy load and improve apps efficiency
         public virtual ICollection<CategoryItem> JoinEntities { get;}
     }
 }
