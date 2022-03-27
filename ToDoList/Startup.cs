ea7c66ce6c21bd8972a7a5524a5bd299a91e3eef
Christen Weston <paleeserecycle@gmail.com>
2022-03-27 11:36:42 -0700

    Updated Startup for Identity

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoList.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace ToDoList
 {
@@ -27,12 +28,21 @@ namespace ToDoList
       services.AddEntityFrameworkMySql()
         .AddDbContext<ToDoListContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ToDoListContext>()
+                .AddDefaultTokenProviders();
     }
 
     public void Configure(IApplicationBuilder app)
     {
       app.UseDeveloperExceptionPage();
+
+      app.UseAuthentication(); 
+
       app.UseRouting();
+
+      app.UseAuthorization();
 
       app.UseEndpoints(routes =>
       {
@@ -40,7 +50,7 @@ namespace ToDoList
       });
 
       app.UseStaticFiles();
-      
+
       app.Run(async (context) =>
       {
         await context.Response.WriteAsync("Hello World!");
